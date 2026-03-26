ff1208144855f4a01e56d56b3b853287b57f7c13
e2402228 <e2402228@edu.vamk.fi>
2026-03-26 21:27:29 +0200

    1.11 doneeeee

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import BaseSelection from "./components/BaseSelection"
 import SummaryBar from "./components/SummaryBar"
+import IngredientSection from "./components/IngredientSection"
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <BaseSelection />
       <SummaryBar />
+      <IngredientSection />
       <main className="flex-1 max-w-6xl w-full mx-auto p-6 flex flex-col gap-8 mt-4" />
       <Footer />
     </div>
---
+++ src/components/IngredientSection.tsx
@@ -0,0 +1,9 @@
+export default function IngredientSection() {
+  return (
+    <div className="bg-zinc-800 rounded-[3rem] p-8 text-white w-full shadow-lg">
+        <div className="rounded-full px-6 py-3 text-black outline-none w-64 border-2 border-transparent focus:border-[#A2D135]">
+            <div className="bg-[#A2D135] text-black font-bold px-6 py-2 rounded-full"></div>
+        </div>
+    </div>
+  );
+};
