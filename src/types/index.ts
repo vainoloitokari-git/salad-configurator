95fcdfc3a33c03fdba592f5171e002f8d87c54cb
e2402235 <vaino.loitokari@gmail.com>
2026-03-26 15:12:48 +0200

    Lisätty src/types/index.ts koodi main kanavalle

---
+++ src/types/index.ts
@@ -0,0 +1,50 @@
+export interface BaseType {
+  id: number;
+  name: string;
+  price?: number;
+  image_url: string;
+  barcode_url: string;
+}
+
+export interface Bowl extends BaseType {
+  base_type_id?: number;
+  volume?: number;
+  slot_count: number;
+  shape: 'round' | 'square';
+}
+
+export interface Category {
+  id: number;
+  name: string;
+  base_type_id?: number;
+}
+
+export interface Ingredient extends BaseType {
+  categoryId: number;
+  diets: string[];
+  weight_grams?: number;
+}
+
+export interface User {
+    id: number;
+    email: string;
+    name?: string;
+    role: string;
+}
+
+export interface Recipe {
+    id: number;
+    userID: number;
+    name: string;
+    bowlID: number;
+    inggredientsIds: number[];
+    slots?: Record<string, Ingredient | null>;
+    is_public?: boolean;
+}
+
+export interface PriceListItem {
+    id: number;
+    item_id: number;
+    price: number;
+    type?: string;
+}
