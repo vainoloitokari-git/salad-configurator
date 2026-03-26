8a75ae4b1191f30bf52044f5f861b7ddfc0b575d
e2402228 <e2402228@edu.vamk.fi>
2026-03-26 21:09:12 +0200

    1.7 doneee

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-const Header = () => {
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import BaseSelection from "./components/BaseSelection"
+import SummaryBar from "./components/SummaryBar"
+
+
+function App() {
+
   return (
-    <div className="bg-zinc-800 text-white w-full h-32 flex justify-between items-start px-8 pt-4">
-        
-        {/* left logo */}
-        <div className="w-24 h-24 rounded-full border-4 border-[#A2D135] flex items-center justify-center flex-col -mt-2 bg-zinc-800 shadow-lg">
-            <span>Fresh Food Factory</span>
-            <span>FRESSE</span>
-        </div>
-
-        {/* center */}
-        <h1 className="text-3xl font-black tracking-widest mt-6">BOWL-LASKURI</h1>
-
-        {/* right logo */}
-        <div className="bg-[#A2D135] text-black rounded-b-3xl rounded-t-xl px-6 py-4 flex flex-col gap-2 min-w-[200px] shadow-md">
-        </div>
-    
+    <div className="min-h-screen flex flex-col bg-white font-sans">
+      <Header />
+      <BaseSelection />
+      <SummaryBar />
+      <main className="flex-1 max-w-6xl w-full mx-auto p-6 flex flex-col gap-8 mt-4" />
+      <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default App
