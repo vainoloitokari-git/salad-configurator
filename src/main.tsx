ceb48110636ebb09b87621d1e616a1c2ddf9cf26
e2402235 <vaino.loitokari@gmail.com>
2026-04-01 18:06:17 +0300

    Taskk 2.12 IngredientSection done

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,10 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />  
-  </StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
