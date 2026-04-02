15e243fe3353b8a46642eebde129a4cf70197eff
Vaino <e2402235@edu.vamk.fi>
2026-04-02 09:40:17 +0300

    Zustand and store

--- src/store/UseIngredientStore.ts
+++ src/store/UseIngredientStore.ts
@@ -15,4 +15,43 @@ interface IngredientStore {
     slots: Record <string, Ingredient | null>;
     baseType: number;
     selectedBowl: Bowl | null;
+
+
+    setBaseType: (id: number) => void;
+    setBowl: (bowl: Bowl | null) => void;
+    clearSelection: () => void;
+    addIngredient: (item: Ingredient) => void;
+    removeIngredient: (id: string) => void;
+
 }
+
+export const useIngredientStore = create<IngredientStore>((set) => ({
+    slots: {},                
+    baseType: 1,              
+    selectedBowl: null,
+
+    setBaseType: (id) =>
+        set(() => ({
+        baseType: id,
+        })),
+
+    setBowl: (bowl) =>
+        set(() => ({
+        selectedBowl: bowl,
+        })),
+
+    clearSelection: () =>
+        set(() => ({
+        slots: {},
+        baseType: 1,
+        selectedBowl: null,
+        })),
+
+  addIngredient: (item) => {
+    console.warn("addIngredient not implemented yet", item);
+  },
+
+  removeIngredient: (id) => {
+    console.warn("removeIngredient not implemented yet", id);
+  },
+}));
