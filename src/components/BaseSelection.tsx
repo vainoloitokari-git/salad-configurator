e9e2e6a67bea2f2df8a01e8eb8399c1b9ba3bcfe
e2402235 <vaino.loitokari@gmail.com>
2026-04-26 22:16:16 +0300

    Task 6.2 Done

--- src/components/BaseSelection.tsx
+++ src/components/BaseSelection.tsx
@@ -1,19 +1,16 @@
-type Ingredient = {
-  id: number;
-  name: string;
-  categoryId: number;
-};
+import { useIngredientStore } from "../store/UseIngredientStore";
+import type { Ingredient } from "../types";
 
-type Props = {
+type BaseSelectionProps = {
   ingredients: Ingredient[];
 };
 
-export default function BaseSelection({ ingredients }: Props) {
-  const baseItems = ingredients;
+export default function BaseSelection({ ingredients }: BaseSelectionProps) {
+  const selectedBase = useIngredientStore((s) => s.baseType);
+  const setBaseType = useIngredientStore((s) => s.setBaseType);
 
   return (
-    <div className="bg-zinc-800 rounded-[3rem] p-6 text-white w-64 flex flex-col gap-4 shadow-lg">
-
+    <div className="bg-[#2f2f2f] text-white rounded-[40px] p-6 w-64 flex flex-col gap-6">
       <div className="text-center">
         <div className="bg-white text-black rounded-full w-8 h-8 flex items-center justify-center mx-auto mb-2">
           2.
@@ -21,14 +18,26 @@ export default function BaseSelection({ ingredients }: Props) {
         <div className="font-semibold">Valitse salaattipohja</div>
       </div>
 
-      {baseItems.map((item) => (
-        <div
+      {ingredients.map((item) => (
+        <button
           key={item.id}
-          className="border-b border-gray-600 pb-2 flex justify-between items-center"
+          onClick={() => setBaseType(item.id)}
+          className={`flex items-center gap-4 cursor-pointer transition ${
+            selectedBase === item.id ? "text-lime-400" : "text-gray-300"
+          }`}
         >
-          <span className="text-sm">{item.name}</span>
-          <div className="w-8 h-8 rounded-full bg-green-400 cursor-pointer"></div>
-        </div>
+          {/* Pyöreä kuva */}
+          <img
+            src={item.image_url}
+            alt={item.name}
+            className="w-14 h-14 rounded-full object-cover bg-[#c2a679]"
+          />
+
+          {/* Nimi */}
+          <div className="whitespace-normal break-words text-left">
+            {item.name}
+          </div>
+        </button>
       ))}
     </div>
   );
